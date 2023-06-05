03620086a1ed33ca48285a27c65ed611e9fb8f93
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-05 15:28:50 -0700

    add header/footer and change themes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,30 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css';
+	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead"><div class="hidden">(icon)</div></svelte:fragment>
+			<h4 class="h4"><a href="/">Disease Visual</a></h4>
+			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<slot />
+
+	<svelte:fragment slot="pageFooter">
+		<ul class="ul pb-5 flex space-x-7 justify-center">
+			<li>
+				<a href="https://github.com/jjoeldaniel/disease_visual">Source Code</a>
+			</li>
+		</ul></svelte:fragment
+	>
+</AppShell>
