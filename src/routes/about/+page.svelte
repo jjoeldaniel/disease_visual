c9397f5992dfb910e17a0e71c5347467069e4a5b
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-20 12:19:10 -0700

    add new routes

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,38 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5 max-w-3xl">
+		<h1 class="h1">Data Science: Disease Outbreak Analysis</h1>
+
+		<h3 class="h3">Group Members</h3>
+		<ul>
+			<li>Joel Daniel Rico</li>
+			<li>Melissa Huynh</li>
+		</ul>
+
+		<h3 class="h3">Description of Work</h3>
+		<p>
+			This project will use data science concepts to visualize various statistics relating to the
+			spread of a disease. We will research the causes, identify a reliable source of data on
+			disease spread, and practice simple deep learning algorithms. We will then clean and prepare
+			the data for visualization, build an accessible website using SvelteKit, and visualize the
+			data in a map, bar chart, line graph, or another type of visualization.
+		</p>
+
+		<h3 class="h3">Steps</h3>
+		<p>
+			Research more about data science concepts. Read articles on disease predictions. Research on
+			causes of foodborne illness. Research data sources. Identify a reliable source of data on
+			disease spread. Practice simple deep learning algorithms. Clean and prepare data. Build an
+			accessible website. Visualize data.
+		</p>
+
+		<h3 class="h3">Tools and Resources</h3>
+		<ul>
+			<li>Data science concepts</li>
+			<li>Articles on disease predictions</li>
+			<li>Reliable source of data on disease spread</li>
+			<li>Data cleaning and preparation tools</li>
+			<li>Web development framework (SvelteKit)</li>
+			<li>Data Visualization tools</li>
+		</ul>
+	</div>
+</div>
