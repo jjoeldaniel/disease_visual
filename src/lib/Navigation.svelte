5a4eb04e9aa6fd97ce98e8904a30c3422a95e705
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-26 01:31:31 -0700

    new Navigation components

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,30 +1,107 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { AppRailTile, drawerStore } from '@skeletonlabs/skeleton';
+	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 
-	function drawerClose(): void {
-		drawerStore.close();
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	function drawerOpen(): void {
+		drawerStore.open({
+			position: 'top'
+		});
 	}
 
-	function about() {
-		goto('/about');
-		drawerClose();
+	let tabsBottomNav = 0;
+	let path = $page.url.pathname;
+	switch (path) {
+		case '/':
+			tabsBottomNav = 0;
+			break;
+		case '/blog':
+			tabsBottomNav = 1;
+			break;
+		case '/projects':
+			tabsBottomNav = 2;
+			break;
+		case '/contact':
+			tabsBottomNav = 3;
+			break;
+		case 'map':
+			tabsBottomNav = 4;
+			break;
+		default:
+			tabsBottomNav = 0;
+	}
+
+	function buttonClick(event: MouseEvent): void {
+		let targetPath = event.target.name;
+		goto(targetPath);
 	}
 </script>
 
-<div class=" h-full flex justify-center items-center">
-	<div class="grid grid-cols-1 gap-3 text-2xl">
-		<AppRailTile on:click={about}
-			><div class="flex place-items-center flex-col">
-				<span class="material-symbols-outlined"> book </span>
-				<h3 class="h3">About</h3>
-			</div></AppRailTile
-		>
-		<AppRailTile on:click={() => goto('https://github.com/jjoeldaniel/disease_visual')}
-			><div class="flex place-items-center flex-col">
-				<span class="material-symbols-outlined"> trophy </span>
-				<h3 class="h3">Source Code</h3>
-			</div></AppRailTile
-		>
-	</div>
-</div>
+<AppBar
+	padding="pt-2 pb-2 md:pt-0 md:pb-0"
+	gridColumns="grid-cols-3"
+	slotDefault="place-self-center"
+	slotTrail="place-content-end"
+>
+	<svelte:fragment slot="lead"
+		><button class="btn btn-sm mr-4 block lg:hidden" on:click={drawerOpen}>
+			<span>
+				<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+					<rect width="100" height="20" />
+					<rect y="30" width="100" height="20" />
+					<rect y="60" width="100" height="20" />
+				</svg>
+			</span>
+		</button>
+		<div class="hidden lg:block">
+			<TabGroup
+				justify="justify-center"
+				active="variant-filled-primary"
+				hover="hover:variant-soft-primary"
+				flex="flex-1 lg:flex-none"
+				rounded=""
+				border=""
+				class="bg-surface-100-800-token w-full"
+			>
+				<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="home" value={0}>
+					<svelte:fragment slot="lead"
+						><span class="material-symbols-outlined"> home </span></svelte:fragment
+					>
+					Home
+				</Tab>
+				<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="map" value={5}>
+					<svelte:fragment slot="lead"
+						><span class="material-symbols-outlined"> map </span></svelte:fragment
+					>
+					Map
+				</Tab>
+				<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="about" value={1}>
+					<svelte:fragment slot="lead"
+						><span class="material-symbols-outlined"> article </span></svelte:fragment
+					>
+					About
+				</Tab>
+				<Tab
+					on:click={() => goto('https://github.com/jjoeldaniel/disease_visual')}
+					bind:group={tabsBottomNav}
+					name="projects"
+					value={2}
+				>
+					<svelte:fragment slot="lead"
+						><span class="material-symbols-outlined"> trophy </span></svelte:fragment
+					>
+					Source Code
+				</Tab>
+			</TabGroup>
+		</div>
+	</svelte:fragment>
+	<h4 class="h4 font-bold gradient-heading"><a href="/">Disease Visualizer</a></h4>
+	<svelte:fragment slot="trail"
+		><div class="pr-3">
+			<LightSwitch />
+		</div></svelte:fragment
+	>
+</AppBar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,7 @@
 	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 
 	import Navigation from '$lib/Navigation.svelte';
+	import NavigationMobile from '$lib/NavigationMobile.svelte';
 
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
@@ -52,70 +53,12 @@
 />
 
 <Drawer>
-	<Navigation />
+	<NavigationMobile />
 </Drawer>
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar
-			padding="pt-2 pb-2 md:pt-0 md:pb-0"
-			gridColumns="grid-cols-3"
-			slotDefault="place-self-center"
-			slotTrail="place-content-end"
-		>
-			<svelte:fragment slot="lead"
-				><button class="btn btn-sm mr-4 block lg:hidden" on:click={drawerOpen}>
-					<span>
-						<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-							<rect width="100" height="20" />
-							<rect y="30" width="100" height="20" />
-							<rect y="60" width="100" height="20" />
-						</svg>
-					</span>
-				</button>
-				<div class="hidden lg:block">
-					<TabGroup
-						justify="justify-center"
-						active="variant-filled-primary"
-						hover="hover:variant-soft-primary"
-						flex="flex-1 lg:flex-none"
-						rounded=""
-						border=""
-						class="bg-surface-100-800-token w-full"
-					>
-						<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="home" value={0}>
-							<svelte:fragment slot="lead"
-								><span class="material-symbols-outlined"> home </span></svelte:fragment
-							>
-							Home
-						</Tab>
-						<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="about" value={1}>
-							<svelte:fragment slot="lead"
-								><span class="material-symbols-outlined"> article </span></svelte:fragment
-							>
-							About
-						</Tab>
-						<Tab
-							on:click={() => goto('https://github.com/jjoeldaniel/disease_visual')}
-							bind:group={tabsBottomNav}
-							name="projects"
-							value={2}
-						>
-							<svelte:fragment slot="lead"
-								><span class="material-symbols-outlined"> trophy </span></svelte:fragment
-							>
-							Source Code
-						</Tab>
-					</TabGroup>
-				</div>
-			</svelte:fragment>
-			<h4 class="h4 font-bold gradient-heading"><a href="/">Disease Visualizer</a></h4>
-			<svelte:fragment slot="trail"
-				><div class="pr-3">
-					<LightSwitch />
-				</div></svelte:fragment
-			>
-		</AppBar>
+		<Navigation />
 	</svelte:fragment>
 	<slot />
 </AppShell>
